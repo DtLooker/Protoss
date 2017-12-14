1a8cddb71d7f6f4c14d2d8029a68d295670b8ec2
DtLooker <dingt90@163.com>
2017-12-14 23:19:56 +0800

    part of cart

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -25,7 +25,7 @@ Page({
      */
     onShow: function () {
         var cartData = cart.getCartDataFromLocal();
-       // var countsInfo = cart.getCartTotalCounts(true);
+        // var countsInfo = cart.getCartTotalCounts(true);
         var cal = this._calcTotalAccountAndCounts(cartData);
 
         this.setData({
@@ -37,16 +37,13 @@ Page({
     },
 
     _calcTotalAccountAndCounts: function (data) {
-        var len = data.length,
-
-            //所需要计算的总价格，需要排除未选中商品
-            account = 0;
-
+        var len = data.length;
+        //所需要计算的总价格，需要排除未选中商品
+        var account = 0;
         //购买商品总个数
-        selectedCounts = 0;
-
+        var selectedCounts = 0;
         //购买商品种类的总数
-        selectedTypeCounts = 0;
+        var selectedTypeCounts = 0;
 
         let multiple = 100;
 
@@ -57,10 +54,58 @@ Page({
                 selectedCounts += data[i].counts;
                 selectedTypeCounts++;
             }
-            return {
-                selectedCounts: selectedCounts,
-                selectedTypeCounts: selectedTypeCounts,
-                account: account / (multiple * multiple)
+        }
+        return {
+            selectedCounts: selectedCounts,
+            selectedTypeCounts: selectedTypeCounts,
+            account: account / (multiple * multiple)
+        }
+    },
+
+    //点击checkbox 
+    toggleSelect: function (event) {
+        var id = cart.getDataSet(event, 'id'),
+            status = cart.getDataSet(event, 'status'),
+            index = this._getProductIndexById(id);
+
+        this.data.cartData[index].selectStatus = !status;
+        this._resetCartData();
+    },
+
+    //是否点击全选
+    toggleSelectAll: function (event) {
+
+        //布尔值status
+        var status = cart.getDataSet(event, 'status') == 'true';
+
+        var data = this.data.cartData,
+            len = data.length;
+        for (let i = 0; i < len; i++) {
+            data[i].selectStatus = !status;
+        }
+        this._resetCartData();
+    },
+
+    _resetCartData: function () {
+        //重新计算总金额和商品总数
+        var newData = this._calcTotalAccountAndCounts(this.data.cartData);
+        this.setData({
+            account: newData.account,
+            selectedCounts: newData.selectedCounts,
+            selectedTypeCounts: newData.selectedTypeCounts,
+            cartData: this.data.cartData
+        });
+    },
+
+
+
+    /* 根据商品id得到 商品所在在下标 */
+    _getProductIndexById: function (id) {
+        var data = this.data.cartData,
+            len = data.length;
+        for (let i = 0; i < len; i++) {
+            if (data[i].id == id) {
+                return i;
             }
         }
     }
